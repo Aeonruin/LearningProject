a341cba6bfbb980cd30b0fa4f3d504c16bc4c807
Aeonruin <MichaelDNails@outlook.com>
2026-04-07 15:40:14 -0700

    Learning all things coding~

---
+++ Phase1/Count Logic wo input.py
@@ -0,0 +1,4 @@
+count = 1
+while count <= 23:
+    print(count)
+    count += 1
---
+++ Phase1/Reinforcement4-7.py
@@ -0,0 +1,8 @@
+h = 1
+while h < 10:
+    input("talk to me dude?")
+    print (h)
+    h +=1
+if h == 10:
+    print( "Complete!")
+
